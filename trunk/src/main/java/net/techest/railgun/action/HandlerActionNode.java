63418adcfc714306b2869e35501aace8168f8de7
baizhongwei.pt@TAOBAO-3C6HTP2X.taobao.ali.com <baizhongwei.pt@TAOBAO-3C6HTP2X.taobao.ali.com>
2012-04-19 15:00:02 +0800

    使用了动态代理加载script handler

--- trunk/src/main/java/net/techest/railgun/action/HandlerActionNode.java
+++ trunk/src/main/java/net/techest/railgun/action/HandlerActionNode.java
@@ -16,14 +16,18 @@
  *  Created on : Apr 9, 2012 , 11:22:50 AM
  *  Author     : princehaku
  */
-
-
 package net.techest.railgun.action;
 
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Iterator;
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
 import net.techest.railgun.system.Handler;
 import net.techest.railgun.system.Resource;
 import net.techest.railgun.system.Shell;
+import net.techest.railgun.util.Configure;
 import net.techest.railgun.util.Log4j;
 import org.dom4j.Element;
 
@@ -31,25 +35,55 @@ import org.dom4j.Element;
  *
  * @author baizhongwei.pt
  */
-public class HandlerActionNode  extends ActionNode {
+public class HandlerActionNode extends ActionNode {
 
     @Override
     public void execute(Element node, Shell shell) throws Exception {
-        
-        // 执行过滤器
-        if (node.getData() != null) {
-            try {
-                Handler filter = (Handler) Class.forName(node.getData().toString()).newInstance();
-                Log4j.getInstance().info("Apply Handler " + node.getData().toString());
-                for (Iterator i = shell.getResources().iterator(); i.hasNext();) {
-                    Resource res = (Resource) i.next();
-                    filter.process(res);
+        // 动态编译和执行脚本
+        if (node.getData() == null) {
+            throw new ActionException("请输入Handler类名");
+        }
+        String className = node.getTextTrim();
+        String scriptDirString = Configure.getSystemConfig().getString("HANDLER_DIR");
+        File scriptDir = new File(scriptDirString);
+        URL[] urls = new URL[]{new URL("file:/" + scriptDir.getAbsolutePath() + "/")};
+        URLClassLoader classLoader = new URLClassLoader(urls, ClassLoader.getSystemClassLoader());
+        Handler handler = null;
+        // 直接加载一次
+        boolean loaded = false;
+        try {
+            handler = (Handler) ( ( classLoader.loadClass(className) ).newInstance() );
+            loaded = true;
+        }
+        catch (Exception ex) {
+            Log4j.getInstance().info(className + "加载失败，尝试动态编译。" + ex.getMessage());
+        }
+        // 加载失败就编译一次
+        try {
+            if (loaded == false) {
+                JavaCompiler javac = ToolProvider.getSystemJavaCompiler();
+                Log4j.getInstance().info("Compile: " + scriptDir.getAbsolutePath() + "/" + className + ".java");
+                int status = javac.run(null, null, null, "-d", scriptDir.getAbsolutePath(), scriptDir.getAbsolutePath() + "/" + className + ".java");
+                if (status != 0) {
+                    throw new ActionException("编译script失败" + status);
                 }
-            } catch (Exception ex) {
-                Log4j.getInstance().error(ex.getMessage());
-                throw new Exception("执行自定义操作失败" + ex.getMessage());
+                handler = (Handler) ( ( classLoader.loadClass(className) ).newInstance() );
+                loaded = true;
             }
         }
-    }
+        catch (Exception ex) {
+            throw new ActionException(className + "动态加载失败" + ex.getMessage());
+        }
 
+        Log4j.getInstance().info("Apply Handler " + className);
+        try {
+            for (Iterator i = shell.getResources().iterator(); i.hasNext();) {
+                Resource res = (Resource) i.next();
+                handler.process(res);
+            }
+        }
+        catch (Exception ex) {
+            throw new ActionException("执行自定义操作失败" + ex.getMessage());
+        }
+    }
 }
