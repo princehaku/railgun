ae26d75296c89c89680ce8d8985fee3d0f94fe32
baizhongwei.pt@TAOBAO-3C6HTP2X.taobao.ali.com <baizhongwei.pt@TAOBAO-3C6HTP2X.taobao.ali.com>
2012-04-19 21:06:21 +0800

    增强动态编译功能的报错

--- trunk/src/main/java/net/techest/railgun/action/HandlerActionNode.java
+++ trunk/src/main/java/net/techest/railgun/action/HandlerActionNode.java
@@ -21,6 +21,7 @@ package net.techest.railgun.action;
 import java.io.File;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.HashMap;
 import java.util.Iterator;
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
@@ -29,6 +30,7 @@ import net.techest.railgun.system.Resource;
 import net.techest.railgun.system.Shell;
 import net.techest.railgun.util.Configure;
 import net.techest.railgun.util.Log4j;
+import net.techest.util.MD5;
 import org.dom4j.Element;
 
 /**
@@ -36,6 +38,12 @@ import org.dom4j.Element;
  * @author baizhongwei.pt
  */
 public class HandlerActionNode extends ActionNode {
+
+    /**
+     * 脚本文件hash
+     *
+     */
+    private HashMap<String, String> fileHashes = new HashMap();
 
     @Override
     public void execute(Element node, Shell shell) throws Exception {
@@ -46,24 +54,35 @@ public class HandlerActionNode extends ActionNode {
         String className = node.getTextTrim();
         String scriptDirString = Configure.getSystemConfig().getString("HANDLER_DIR");
         File scriptDir = new File(scriptDirString);
+        
+        String filePath = scriptDir.getAbsolutePath() + "/" + className + ".java";
+        File file = new File(filePath);
         URL[] urls = new URL[]{new URL("file:/" + scriptDir.getAbsolutePath() + "/")};
+        Log4j.getInstance().info("脚本目录  file:/" + scriptDir.getAbsolutePath() + "/");
         URLClassLoader classLoader = new URLClassLoader(urls, ClassLoader.getSystemClassLoader());
         Handler handler = null;
         // 直接加载一次
         boolean loaded = false;
-        try {
-            handler = (Handler) ( ( classLoader.loadClass(className) ).newInstance() );
-            loaded = true;
-        }
-        catch (Exception ex) {
-            Log4j.getInstance().info(className + "加载失败，尝试动态编译。" + ex.getMessage());
+        // 不在hash里的有更新的文件则重新编译
+        String newHash = MD5.getMD5(( file.getName() + file.length() + file.lastModified() ).getBytes());
+        if (fileHashes.containsKey(file.getName()) && fileHashes.get(file.getName()).equals(newHash)) {
+            try {
+                handler = (Handler) ( ( classLoader.loadClass(className) ).newInstance() );
+                loaded = true;
+            }
+            catch (Exception ex) {
+                Log4j.getInstance().info(className + "加载失败，尝试动态编译。" + ex.getMessage());
+            }
         }
         // 加载失败就编译一次
         try {
             if (loaded == false) {
                 JavaCompiler javac = ToolProvider.getSystemJavaCompiler();
-                Log4j.getInstance().info("Compile: " + scriptDir.getAbsolutePath() + "/" + className + ".java");
-                int status = javac.run(null, null, null, "-d", scriptDir.getAbsolutePath(), scriptDir.getAbsolutePath() + "/" + className + ".java");
+                Log4j.getInstance().info("Compile: " + filePath);
+                if (javac == null) {
+                    throw new ActionException("javac无法获取");
+                }
+                int status = javac.run(null, null, null, "-d", scriptDir.getAbsolutePath(), filePath);
                 if (status != 0) {
                     throw new ActionException("编译script失败" + status);
                 }
@@ -72,6 +91,7 @@ public class HandlerActionNode extends ActionNode {
             }
         }
         catch (Exception ex) {
+            ex.printStackTrace();
             throw new ActionException(className + "动态加载失败" + ex.getMessage());
         }
 
