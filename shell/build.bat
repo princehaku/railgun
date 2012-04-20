36ac6b1e8c878d5de83a85ac1d154255893ea17d
baizhongwei.pt@TAOBAO-3C6HTP2X.taobao.ali.com <baizhongwei.pt@TAOBAO-3C6HTP2X.taobao.ali.com>
2012-04-20 11:20:46 +0800

    build shell更新

--- shell/build.bat
+++ shell/build.bat
@@ -1,6 +1,6 @@
 @echo off
-rm railgun -rf
-hg clone http://hg.code.sf.net/p/railgun/code railgun
+rmdir /S /Q railgun
+hg clone http://hg.code.sf.net/p/railgun/code railgun || (echo "Please Install hg "; exit 0;)
 cd railgun/trunk
-mvn install:install-file -DgroupId=wltea -DartifactId=IKAnalyzer -Dversion=2012 -Dpackaging=jar -Dfile=lib/IKAnalyzer-2012.jar
-mvn install
+mvn install:install-file -DgroupId=wltea -DartifactId=IKAnalyzer -Dversion=2012 -Dpackaging=jar -Dfile=lib/IKAnalyzer-2012.jar && mvn install
+echo "安装完成 请使用run.bat运行"
--- shell/build.sh
+++ shell/build.sh
@@ -1,6 +1,6 @@
-Ôªø#!/bin/bash
+#!/bin/bash
 rm railgun -rf
-hg clone http://hg.code.sf.net/p/railgun/code railgun
+hg clone http://hg.code.sf.net/p/railgun/code railgun || (echo "Please Install hg "; exit 0;)
 cd railgun/trunk
-mvn install:install-file -DgroupId=wltea -DartifactId=IKAnalyzer -Dversion=2012 -Dpackaging=jar -Dfile=lib/IKAnalyzer-2012.jar
-mvn install
+mvn install:install-file -DgroupId=wltea -DartifactId=IKAnalyzer -Dversion=2012 -Dpackaging=jar -Dfile=lib/IKAnalyzer-2012.jar && mvn install
+echo "∞≤◊∞ÕÍ≥… «Î π”√run.sh‘À––"
