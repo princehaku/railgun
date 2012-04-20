6da6fe970e9921e7e0ae0a37ba6c8bd5811c2b0b
baizhongwei.pt@TAOBAO-3C6HTP2X.taobao.ali.com <baizhongwei.pt@TAOBAO-3C6HTP2X.taobao.ali.com>
2012-04-20 11:32:20 +0800

    运行脚本优化

--- trunk/run.sh
+++ trunk/run.sh
@@ -1,6 +1,11 @@
-﻿#!/bin/bash
-if [ $JAVA_HOME eq "" ] ;
- echo "Please Set JAVA_HOME To Jdk Dir"
- javacpath = `ls -ls /usr/bin/javac | awk -F '->' '{print $2}'`
-if
-"$JAVA_HOME" -jar target/railgun-0.1.jar
+#!/bin/bash
+if [ "${JAVA_HOME-null}" == "null" ]; then
+    echo "Please Set JAVA_HOME To Jdk Dir";
+    javacpath=`ls -ls /usr/bin/javac | awk -F '->' '{print $2}'`;
+    JAVA_HOME=${javacpath/bin\/javac//}
+    if [ "${JAVA_HOME-null}" == "null" ]; then
+        echo "JAVA_HOME Auto Set Failed";
+        exit 0;
+    fi
+fi
+$JAVA_HOME/bin/java -jar target/railgun-0.1.jar
