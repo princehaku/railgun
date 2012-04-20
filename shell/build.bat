4836f3d48bc482d8a76107f6e23d929ebafd9110
baizhongwei.pt@TAOBAO-3C6HTP2X.taobao.ali.com <baizhongwei.pt@TAOBAO-3C6HTP2X.taobao.ali.com>
2012-04-20 10:41:53 +0800

    添加构建和运行脚本

---
+++ shell/build.bat
@@ -0,0 +1,6 @@
+@echo off
+rm railgun -rf
+hg clone http://hg.code.sf.net/p/railgun/code railgun
+cd railgun/trunk
+mvn install:install-file -DgroupId=wltea -DartifactId=IKAnalyzer -Dversion=2012 -Dpackaging=jar -Dfile=lib/IKAnalyzer-2012.jar
+mvn install
---
+++ trunk/run.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+if [ $JAVA_HOME eq "" ] ;
+ echo "Please Set JAVA_HOME To Jdk Dir"
+ javacpath = `ls -ls /usr/bin/javac | awk -F '->' '{print $2}'`
+if
+"$JAVA_HOME" -jar target/railgun-0.1.jar
