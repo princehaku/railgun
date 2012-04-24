54c9e9efa33ba4b1bc9e8320f8a73105f4c03f2e
baizhongwei.pt@TAOBAO-3C6HTP2X.taobao.ali.com <baizhongwei.pt@TAOBAO-3C6HTP2X.taobao.ali.com>
2012-04-24 19:33:13 +0800

    删除hg存在判断

--- shell/build.sh
+++ shell/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm railgun -rf
-hg clone http://hg.code.sf.net/p/railgun/code railgun || (echo "Please Install hg "; exit 0;)
+hg clone http://hg.code.sf.net/p/railgun/code railgun
 cd railgun/trunk
 sh -c "mvn install:install-file -DgroupId=wltea -DartifactId=IKAnalyzer -Dversion=2012 -Dpackaging=jar -Dfile=lib/IKAnalyzer-2012.jar"
 sh -c "mvn install"
