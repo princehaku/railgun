5c9a1bb2e74603d721893aa2f1bcc01c2173f049
baizhongwei.pt@TAOBAO-3C6HTP2X.taobao.ali.com <baizhongwei.pt@TAOBAO-3C6HTP2X.taobao.ali.com>
2012-05-09 15:56:50 +0800

    db接口完成

--- trunk/src/main/java/net/techest/railgun/rest/DBHandler.java
+++ trunk/src/main/java/net/techest/railgun/rest/DBHandler.java
@@ -15,15 +15,19 @@
  *  Created on : May 1, 2012, 3:14:34 PM
  *  Author     : princehaku
  */
-
 package net.techest.railgun.rest;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import net.techest.railgun.db.DBConnection;
 import net.techest.railgun.db.DBException;
 import net.techest.railgun.net.QuestParams;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Fieldable;
 
 /**
  *
@@ -33,11 +37,38 @@ public class DBHandler implements APIHandlerInterface {
 
     @Override
     public void handle(QuestParams requestParams, JSONObject responseJson) {
+        if (requestParams.get("key") == null || requestParams.get("db") == null || requestParams.get("formName") == null
+                || requestParams.get("value") == null || requestParams.get("cols") == null) {
+            responseJson.put("errmsg", "Method Not Support");
+            return;
+        }
+        
+        DBConnection dbc = null;
         try {
-            DBConnection dbc = new DBConnection(requestParams.get("db"));
-        } catch (DBException ex) {
+            dbc = new DBConnection(requestParams.get("db"));
+            ResultSet rs = dbc.getResultSet(requestParams.get("formName"), new String[]{requestParams.get("key")}, new String[]{requestParams.get("value")});
+
+            JSONArray response = new JSONArray();
+            while (rs.next()) {
+                JSONObject enty = new JSONObject();
+                String[] cols = requestParams.get("cols").split(",");
+                for (String col : cols) {
+                    enty.put(col, rs.getString(col));
+                }
+                response.add(enty);
+            }
+
+            responseJson.put("content", response);
+
+            dbc.getConnection().close();
+        }
+        catch (DBException ex) {
             responseJson.put("errmsg", ex.getMessage());
+            return;
+        }
+        catch (SQLException ex) {
+            responseJson.put("errmsg", ex.getMessage());
+            return;
         }
     }
-
 }
