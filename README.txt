3ba94c3dc754803d07941d9d5044380e78d57af3
princehaku <baizhongwei@gmail.com>
2012-11-13 19:43:56 +0800

    linux版构建脚本 以及说明文件更新

---
+++ README.txt
@@ -0,0 +1,5 @@
+Please make sure you have installed mvn and jdk
+make sure those command are executable from your terminal
+	mvn java javac
+and make sure you add JAVA_HOME point to your jdk
+	set JAVA_HOME=C:\Java\jdk1.7.0_09\
--- build_demo_vsearch/build.sh
+++ build_demo_vsearch/build.sh
@@ -1,12 +1,32 @@
-#!/bin/bash
-rm railgun -rf
-git clone git://github.com/princehaku/railgun.git railgun
-cd railgun/trunk
+#!/bin/sh
+echo "+++++++++++++++++++++++"
+echo "   building starting   "
+echo "makesure mvn executable"
+echo "+++++++++++++++++++++++"
+basepath=$(pwd)
+rm dist -rf
+mkdir -p dist/lib/
+cd ../railgun_core/
 sh -c "mvn install:install-file -DgroupId=wltea -DartifactId=IKAnalyzer -Dversion=2012 -Dpackaging=jar -Dfile=lib/IKAnalyzer-2012.jar"
-sh -c "mvn install"
-cd ../../
-mv -f railgun/shell/deploy.sh ./
-echo "+++++++++++++++++++++"
-echo "build done"
-echo "+++++++++++++++++++++"
-read
+sh -c "mvn clean install"
+cp target/lib/*.jar $basepath/dist/lib/
+cp target/*.jar $basepath/dist/
+echo "+++++++++++++++++++++++"
+echo "build railgun core done"
+echo "+++++++++++++++++++++++"
+cd ../demo_vsearch/
+sh -c "mvn clean install"
+mkdir -p ${basepath}/dist/src/main/resources/
+cp src/main/resources ${basepath}/dist/src/main/resources/ -R
+rm $basepath/dist/src/main/resources/filters/*.class
+cp src/main/java/system.propertie $basepath/dist/
+cp target/*.jar $basepath/dist/
+cd $basepath/dist/
+echo "+++++++++++++++++++++++"
+echo "   build vsearch done  "
+echo "+++++++++++++++++++++++"
+echo "#!/bin/sh"> run.sh
+echo "${JAVA_HOME}/bin/java -Dfile.encoding=UTF-8 -classpath ./lib/*;./* net.techest.vsearch.App">> run.sh
+echo "+++++++++++++++++++++++"
+echo "   using dist\run.sh  "
+echo "+++++++++++++++++++++++"
